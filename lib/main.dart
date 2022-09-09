6b079f35d18c9cea4f0511c39e77482287bfee76
KalitovStudy <nikita.kalitov.study@gmail.com>
2022-09-09 18:43:38 +0400

    Добавлен 7 раздел

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'laba_3_2/main.dart' as laba_3_2;
 import 'laba_4/main.dart' as laba_4;
 import 'laba_5/main.dart' as laba_5;
+import 'laba_7/main.dart' as laba_7;
 
 void main() {
   laba_5.main();
